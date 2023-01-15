c5ffb74b5477e32cb0b9920314ac93124977f323
MaximePeterburg <getmax4444@gmail.com>
2023-01-15 11:02:38 +0300

    added categories component, and styling

---
+++ src/components/WebShop/WebShop.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Categories from "./Categories/Categories";
+
+function WebShop() {
+  return (
+    <div>
+      <Categories />
+    </div>
+  );
+}
+
+export default WebShop;
